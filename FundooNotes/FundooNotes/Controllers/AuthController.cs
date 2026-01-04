b8cdb3ed3e296555a280e3cdb45dcc19219caedd
alamdilshad87 <dilshadand@gmail.com>
2026-01-05 03:15:26 +0530

    AuthCOntroller Register Post Request

---
+++ FundooNotes/FundooNotes/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Interfaces;
+using ModelLayer.DTOs.Auth;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooNotes.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+        {
+            await _authService.RegisterAsync(dto);
+            return Ok(new { Message = "User Register Successfully" });
+        } 
+    }
+}
