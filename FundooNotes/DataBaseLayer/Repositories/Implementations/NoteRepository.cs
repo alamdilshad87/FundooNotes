0dfd97cc0b9707d476f969098745c773c033d007
alamdilshad87 <dilshadand@gmail.com>
2026-01-07 14:00:49 +0530

    Added Note Repository Interface and Implementation

---
+++ FundooNotes/DataBaseLayer/Repositories/Implementations/NoteRepository.cs
@@ -0,0 +1,51 @@
+﻿using DataBaseLayer.Context;
+using DataBaseLayer.Entities;
+using DataBaseLayer.Repositories.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseLayer.Repositories.Implementations
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly FundooNotesDbContext _context;
+
+        public NoteRepository(FundooNotesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Note note)
+        {
+            await _context.Notes.AddAsync(note);
+        }
+
+        public async Task<List<Note>> GetAllByUserAsync(int userId)
+        {
+            return await _context.Notes
+                .Where(n => n.UserId == userId && !n.IsDeleted)
+                .OrderByDescending(n => n.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<Note?> GetByIdAsync(int noteId, int userId)
+        {
+            return await _context.Notes
+                .FirstOrDefaultAsync(n =>
+                    n.NoteId == noteId &&
+                    n.UserId == userId &&
+                    !n.IsDeleted);
+        }
+
+        public Task UpdateAsync(Note note)
+        {
+            _context.Notes.Update(note);
+            return Task.CompletedTask;
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ FundooNotes/DataBaseLayer/Repositories/Interfaces/INoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataBaseLayer.Entities;
+
+namespace DataBaseLayer.Repositories.Interfaces
+{
+    public interface INoteRepository
+    {
+        Task AddAsync(Note note);
+        Task<List<Note>> GetAllByUserAsync(int userId);
+        Task<Note?> GetByIdAsync(int noteId, int userId);
+        Task UpdateAsync(Note note);
+        Task SaveAsync();
+    }
+}
